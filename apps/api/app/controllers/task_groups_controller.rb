a3c2b35016bb0f9acbcabb359ecdf1b6700f7b87
William DA SILVA <william.da.silva@outlook.com>
2021-04-15 02:28:58 +0200

    fix: perf issue with task groups

--- apps/api/app/controllers/task_groups_controller.rb
+++ apps/api/app/controllers/task_groups_controller.rb
@@ -2,7 +2,7 @@ class TaskGroupsController < ApplicationController
 
   api :GET, '/task_groups'
   def index
-    @task_groups = TaskGroup.includes(:tasks, :user).order(created_at: :desc).page(params[:page])
+    @task_groups = TaskGroup.includes(:tasks, :user).joins(:user, :tasks).order(created_at: :desc).page(params[:page])
 
     render 'task_groups/index'
   end
