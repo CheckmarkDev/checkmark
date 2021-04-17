b204ac01177f696d5488a50264973b769eab1ea9
William DA SILVA <william.da.silva@outlook.com>
2021-04-17 21:22:43 +0200

    feat(api): add comment email

--- apps/api/app/mailers/task_mailer.rb
+++ apps/api/app/mailers/task_mailer.rb
@@ -13,4 +13,19 @@ class TaskMailer < ApplicationMailer
       format.mjml
     end
   end
+
+  def comment (task, user, comment)
+    @name = "#{user.first_name} #{user.last_name}"
+    @username = user.username
+    @task_content = task.content
+    @task_username = task.user.username
+    @task_uuid = task.uuid
+    @task_state = task.state
+    @comment_content = comment.content
+
+    mail(to: task.user.email, subject: "#{user.username} a commenté votre tâche !") do |format|
+      format.text
+      format.mjml
+    end
+  end
 end
