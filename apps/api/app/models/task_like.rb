be9f649c7bc53362f838c28dccabfbb0bc5e5328
William DA SILVA <william.da.silva@outlook.com>
2021-04-17 21:28:41 +0200

    feat: send email whenever like or comment occurs

--- apps/api/app/models/task_like.rb
+++ apps/api/app/models/task_like.rb
@@ -2,10 +2,17 @@ class TaskLike < ApplicationRecord
   belongs_to :user
   belongs_to :task
 
+  after_create :send_like_email
+
   enum state: [
     :active,
     :inactive
   ]
 
   scope :from_user, -> (user) { where(user: user) }
+
+  private
+    def send_like_email
+      TaskMailer.like(self.task, self.user).deliver_later
+    end
 end
