2acc5b973c9643bd050027f07a1ff8827826b6e8
William DA SILVA <william.da.silva@outlook.com>
2021-04-26 14:47:48 +0200

    feat: setting up discord bot (#20)

--- apps/discord-bot/index.js
+++ apps/discord-bot/index.js
@@ -59,18 +59,15 @@ server.post('/webhooks', async (req, res) => {
         return res.json({ status: 400, errorMessage: 'bad parameter' })
     }
 
-    const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_EVENT);
-    let message
-
-    const { first_name, last_name, username, avatar_url } = data.user
-    const fullName = `${first_name} ${last_name} (${username})`
-    const userProfileUrl = `https://www.checkmark.dev/u/${username}`
-
     switch (event) {
-        case 'task.created':
+        case 'task.created': {
             const { url, state, content } = data
+            const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_EVENT);
+            const { first_name, last_name, username, avatar_url } = data.user
+            const fullName = `${first_name} ${last_name} (${username})`
+            const userProfileUrl = `https://www.checkmark.dev/u/${username}`
 
-            message = new MessageEmbed()
+            const message = new MessageEmbed()
                 .setColor(getStateColor(state))
                 .setAuthor(fullName, avatar_url, userProfileUrl)
                 .setTitle(`Création d\'une nouvelle tâche`)
@@ -80,16 +77,34 @@ server.post('/webhooks', async (req, res) => {
                 .setTimestamp();
 
             await channel.send(message)
-            break
-        case 'weekly_summary':
-            message = new MessageEmbed()
+        } break
+        case 'weekly_summary.created': {
+            const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_EVENT);
+            const { first_name, last_name, username, avatar_url } = data.user
+            const fullName = `${first_name} ${last_name} (${username})`
+            const userProfileUrl = `https://www.checkmark.dev/u/${username}`
+
+            const message = new MessageEmbed()
                 .setAuthor(fullName, avatar_url, userProfileUrl)
                 .setTitle(`Résumé de la semaine`)
                 .setFooter('Checkmark', 'https://www.checkmark.dev/icon.png')
                 .setTimestamp();
 
             await channel.send(message)
-            break
+        } break
+        case 'changelog.created': {
+            const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_CHANGELOG);
+            const { name, content, url, created_at } = data
+
+            const message = new MessageEmbed()
+                .setTitle(name)
+                .setURL(url)
+                .setDescription(content)
+                .setFooter('Changelog', 'https://www.changelog.xyz/icon.png')
+                .setTimestamp(Date.parse(created_at));
+
+            await channel.send(message)
+        } break
     }
 
     res.send('coucou');
