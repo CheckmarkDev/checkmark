7c4c0e67464d055e0f8bc8cbd9d47ce2d6f79cb4
Rémy Mulet <dvs75019@gmail.com>
2021-05-02 17:15:24 +0200

    Add new table projects_tasks

--- apps/api/app/models/project.rb
+++ apps/api/app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :tasks
 end
--- apps/api/db/schema.rb
+++ apps/api/db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_214628) do
+ActiveRecord::Schema.define(version: 2021_05_02_135533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2021_04_24_214628) do
     t.index ["slug"], name: "index_projects_on_slug", unique: true
     t.index ["user_id"], name: "index_projects_on_user_id"
     t.index ["uuid"], name: "index_projects_on_uuid", unique: true
+  end
+
+  create_table "projects_tasks", id: false, force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "task_id"
+    t.index ["project_id"], name: "index_projects_tasks_on_project_id"
+    t.index ["task_id"], name: "index_projects_tasks_on_task_id"
   end
 
   create_table "streaks", force: :cascade do |t|
