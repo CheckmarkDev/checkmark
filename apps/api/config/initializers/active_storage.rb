b2baf903a525998ea681e9c788725973aa31894e
William DA SILVA <william.da.silva@outlook.com>
2021-05-22 23:24:14 +0200

    feat(api): override the active storage attachment model to include uuid on create

--- apps/api/config/initializers/active_storage.rb
+++ apps/api/config/initializers/active_storage.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
+module ActiveStorageAttachmentList
+  extend ActiveSupport::Concern
+
+  included do
+    after_initialize :generate_uuid, unless: :uuid?
+
+    def generate_uuid
+      self.uuid = SecureRandom.uuid if defined? uuid
+    end
+
+    private
+
+    def uuid?
+      has_attribute?(:uuid) && uuid.present?
+    end
+  end
+end
+
+Rails.configuration.to_prepare do
+  ActiveStorage::Attachment.send :include, ActiveStorageAttachmentList
+end
+
 ActiveStorage::Current.host = ENV.fetch('ASSET_HOST', 'https://api.checkmark.dev')
