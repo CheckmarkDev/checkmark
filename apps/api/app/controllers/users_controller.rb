5a4da476fa56a7a8e0f10f9f7e5849315e5e329d
William DA SILVA <william.da.silva@outlook.com>
2021-05-24 00:36:26 +0200

    feat(api): add endpoint to fetch random users

--- apps/api/app/controllers/users_controller.rb
+++ apps/api/app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
   api :GET, '/users/:username'
   def show; end
 
+  api :GET, '/users/random'
+  def random
+    @users = User.includes([avatar_attachment: :blob]).order("RANDOM()").limit(10).page(params[:page])
+
+    render "users/index", status: :ok
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
