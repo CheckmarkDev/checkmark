24541c05f5594eb60a87bb6148852b2ceb8adf9a
William DA SILVA <william.da.silva@outlook.com>
2021-05-26 01:44:07 +0200

    feat(api): add migration to add main color column

---
+++ apps/api/db/migrate/20210525232157_add_main_color_to_project.rb
@@ -0,0 +1,9 @@
+class AddMainColorToProject < ActiveRecord::Migration[6.1]
+  def up
+    add_column :projects, :main_color, :string, default: '#2589C7', null: false
+  end
+
+  def down
+    remove_column :projects, :main_color
+  end
+end
