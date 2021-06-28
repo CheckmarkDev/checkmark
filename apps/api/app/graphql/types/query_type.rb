372c25716c2992de3e72957c7f40bd85003e8a8b
William DA SILVA <william.da.silva@outlook.com>
2021-06-28 21:00:08 +0200

    feat(api): add random user query

--- apps/api/app/graphql/types/query_type.rb
+++ apps/api/app/graphql/types/query_type.rb
@@ -10,9 +10,21 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :all_links, [Types::LinkType], null: false, description: 'Return all links'
+    field :random_users, [Types::UserType], null: false, description: 'Return 10 random users'
 
     def all_links
       Link.order(created_at: :desc).all
+    end
+
+    def random_users
+      users = Rails.cache.fetch('random_users', expires_in: 24.hours) do
+        return User.where.not(status: User.statuses[:blocked])
+          .includes([avatar_attachment: :blob])
+          .order('RANDOM()')
+          .limit(10)
+      end
+
+      return users
     end
   end
 end
